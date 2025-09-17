401166bba743d0431ea1f04997bf5333fc836c94
Hhimanshhu <himanshumagar8088@gmail.com>
2025-09-17 13:02:46 +0530

    refactor: Clean up db.ts by removing commented-out code for improved readability

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,4 @@
+// lib/db.ts
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL as string;
@@ -13,9 +14,8 @@ interface MongooseCache {
   promise: Promise<Mongoose> | null;
 }
 
-// 👇 Ensure we extend NodeJS globalThis properly
+// Extend the global object to include _mongoose
 declare global {
-  // eslint-disable-next-line no-var
   var _mongoose: MongooseCache | undefined;
 }
 
@@ -44,6 +44,53 @@ export async function connectToDatabase(): Promise<Mongoose> {
 
   return cached.conn;
 }
+
+// import mongoose, { Mongoose } from "mongoose";
+
+// const MONGODB_URL = process.env.MONGODB_URL as string;
+
+// if (!MONGODB_URL) {
+//   throw new Error(
+//     "Please define the MONGODB_URL environment variable inside .env.local"
+//   );
+// }
+
+// interface MongooseCache {
+//   conn: Mongoose | null;
+//   promise: Promise<Mongoose> | null;
+// }
+
+// // 👇 Ensure we extend NodeJS globalThis properly
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var _mongoose: MongooseCache | undefined;
+// }
+
+// // Use global cache if available
+// const cached: MongooseCache = global._mongoose ?? { conn: null, promise: null };
+// global._mongoose = cached;
+
+// export async function connectToDatabase(): Promise<Mongoose> {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//       maxPoolSize: 10,
+//     };
+
+//     cached.promise = mongoose.connect(MONGODB_URL, opts);
+//   }
+
+//   try {
+//     cached.conn = await cached.promise;
+//   } catch (e) {
+//     cached.promise = null;
+//     throw e;
+//   }
+
+//   return cached.conn;
+// }
 
 
 
